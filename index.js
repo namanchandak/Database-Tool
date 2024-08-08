be04a9b470690331bf174790eca6f66e5510e3a9
Naman Chandak <naman.chandak@hotmail.com>
2024-08-08 13:10:18 +0530

    where clause

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.listen(8080, () => {
 const serverRoute = require('../DB tool/src/route/server.route')
 const join = require('../DB tool/src/route/join.route')
 const jsonData = require('../DB tool/src/route/jsonData.route')
+const whereClause = require('../DB tool/src/route/whereClause.router')
 
 app.use("/", serverRoute)
 app.use("/join", join)
+app.use("/where", whereClause)
 app.use("/jsonData", jsonData)
 
