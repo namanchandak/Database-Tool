384ae29caac096d871bfc31721694a83a85f92d9
Naman Chandak <naman.chandak@hotmail.com>
2024-08-20 16:09:41 +0530

    join implemented with for loop

--- src/controller/join.controller.js
+++ src/controller/join.controller.js
@@ -1,46 +1,18 @@
 const pool = require('../config/config');
 const fs = require('fs');
 const path = require('path');
-
 const configPath = path.join(__dirname, '../config/dbconfig.json');
 let config;
+
 try {
     config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 } catch (err) {
     console.error('Error reading config file:', err);
 }
 
-const buildJoin = (conds) => {
-    return conds.map(({ joinType, table1, table2, conditions }, index) => {
-        let clause = "";
-
-        if (conditions) {
-            const nestedClause = buildJoin(conditions);
-            clause = `${nestedClause}`;
-        }
-        else {
-            const [tableName, attributeName] = table2.split('.');
-
-            clause += `${tableName}`
-        }
-        if (joinType) {
-            const [tableName, attributeName] = table1.split('.');
-
-            clause += ` ${joinType} ${tableName} ON ${table1} =  ${table2}`;
-        }
-        return clause;
-    }).join(' ');
-}
-
 const join = async (req, res) => {
-
-
-    try {
-        
-        const { joinsHere } = req.body;
-        const selectColumns = joinsHere.map(join => join.selectColumns).flat();
-
-
+    try {   
+        const { joinsHere,selectColumns } = req.body;
         if (!selectColumns || !Array.isArray(selectColumns)) {
             throw new Error('Missing or invalid required field: selectColumns');
         }
@@ -48,29 +20,38 @@ const join = async (req, res) => {
             throw new Error('Configuration not loaded correctly');
         }
         const tableNames = Array.from(new Set(selectColumns.map(col => col.split('.')[0])));
-
         if (!tableNames.length) {
             throw new Error('No valid table names found in selectColumns');
         }
-
-
-        const joinQuery = buildJoin(joinsHere);
-        const selectColumnsString = selectColumns.join(', ');
-        const connection = await pool.getConnection();
-        const query = `select ${selectColumnsString} from  ${joinQuery}`
-
-        try {
-            console.log('Executing Query:', query);
-            const [results] = await connection.execute(query);
-            res.json(results);
-        } finally {
-            connection.release();
+        let query = `select ${selectColumns} from `
+        let joinClause = '';
+        for (let i = 0; i < joinsHere.length; i++) {
+            const { table1, table2, joinType } = joinsHere[i];
+            if (!table1 || !table2 || !joinType) {
+                throw new Error('Missing required field(s) in join configuration');
+            }
+            const [table1Name, attribute1Name] = table1.split('.');
+            const [table2Name, attribute2Name] = table2.split('.');
+            if (i === 0) {
+                joinClause += ` ${table1Name} ${joinType} ${table2Name} ON ${table1} = ${table2} `;
+            } else {
+                joinClause += ` ${joinType} ${table2Name} ON ${table1} = ${table2} `;
+            }
         }
+        query+= joinClause
+        console.log(query ,"\n", joinsHere, "\n" , joinClause)
+        const connection = await pool.getConnection();
+            try {
+                console.log('Executing Query:', query);
+                const [results] = await connection.execute(query);
+                res.json(results);
+            } finally {
+                connection.release();
+            }
     }
     catch (error) {
         console.error('Error in join:', error.message);
         res.status(500).json({ error: 'Internal Server Error', message: error.message });
     }
 }
-
 module.exports = { join }
